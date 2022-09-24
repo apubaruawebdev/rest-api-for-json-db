3253a68d926b9209ac21d966e4114805c327fa1d
Apu-Barua <apubarua.cse@gmail.com>
2022-09-24 20:03:46 +0600

    Upload project

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+// init environment variable
+const PORT = process.env.PORT || 8080;
+
+// init express
+const app = express();
+
+// express middlewares
+app.use(express.json());
+app.use(express.urlencoded( { extended : false } ));
+
+
+// server listen
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${ PORT }`.bgGreen.black);
+})
